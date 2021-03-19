552466731c728f749b903e6b91ccf8d2579873b6
RayenD <zufirka@icloud.com>
2021-03-19 19:32:58 +0300

    Lesson4

---
+++ UIKitt_objC/UIKitt_objC/LocationManager.h
@@ -0,0 +1,15 @@
+//
+//  LocationManager.h
+//  UIKitt_objC
+//
+//  Created by Rayen on 08.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+- (void)start;
+- (void)stop;
+
+@end
